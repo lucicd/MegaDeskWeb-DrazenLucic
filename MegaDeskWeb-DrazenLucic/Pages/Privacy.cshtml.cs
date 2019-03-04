54a35e7dbcdc0f6e769925a48fdfb2327e2835f4
Drazen Lucic <lucicd@hotmail.com>
2019-03-04 20:00:30 +0400

    Add project files.

---
+++ MegaDeskWeb-DrazenLucic/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MegaDeskWeb_DrazenLucic.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

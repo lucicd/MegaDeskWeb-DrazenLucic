7a442ee86d4132527a73228a40e922974016cebb
Drazen Lucic <lucicd@hotmail.com>
2019-03-08 08:36:58 +0400

    Searching and sorting for DeskQuotes

--- MegaDeskWeb-DrazenLucic/Pages/DeskQuotes/Index.cshtml.cs
+++ MegaDeskWeb-DrazenLucic/Pages/DeskQuotes/Index.cshtml.cs
@@ -18,17 +18,46 @@ namespace MegaDeskWeb_DrazenLucic.Pages.DeskQuotes
             _context = context;
         }
 
+        public string NameSort { get; set; }
+        public string MaterialSort { get; set; }
+        public string CurrentFilter { get; set; }
+
         public PaginatedList<DeskQuote> DeskQuote { get;set; }
 
-        public async Task OnGetAsync(int? pageIndex)
+        public async Task OnGetAsync(string sortOrder, string searchString, int? pageIndex)
         {
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            MaterialSort = sortOrder == "material" ? "material_desc" : "material";
+            CurrentFilter = searchString;
+
             var deskQuotes = from m in _context.DeskQuote
-                             orderby m.ID descending
                              select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                deskQuotes = deskQuotes.Where(s => s.Customer.Contains(searchString)
+                                       || s.SurfaceMaterial.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    deskQuotes = deskQuotes.OrderByDescending(s => s.Customer);
+                    break;
+                case "material_desc":
+                    deskQuotes = deskQuotes.OrderByDescending(s => s.SurfaceMaterial);
+                    break;
+                case "material":
+                    deskQuotes = deskQuotes.OrderBy(s => s.SurfaceMaterial);
+                    break;
+                default:
+                    deskQuotes = deskQuotes.OrderBy(s => s.Customer);
+                    break;
+            }
+
             int pageSize = 5;
             DeskQuote = await PaginatedList<DeskQuote>.CreateAsync(
                 deskQuotes.AsNoTracking(), pageIndex ?? 1, pageSize);
-            //DeskQuote = await deskQuotes.ToListAsync();
         }
     }
 }
